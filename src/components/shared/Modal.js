40b2194d3b43dd8d7e005c9af190a5fe0174f6b2
HimeurWail <kw_himeur@esi.dz>
2023-05-27 00:24:21 +0100

    form fixed and new depandancy

--- src/components/shared/Modal.js
+++ src/components/shared/Modal.js
@@ -1,10 +1,10 @@
 
-function Model({ onClose, Children }){
+function Modal({ onClose, children }){
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center backdrop-blur-md bg-black bg-opacity-50">
       <div className="bg-white rounded-lg shadow-lg p-4">
-        {Children}
+        {children}
         <button
           className="bg-gray-500 text-white px-4 py-2 rounded-lg hover:bg-gray-600 float-right"
           onClick={onClose}
@@ -16,4 +16,4 @@ function Model({ onClose, Children }){
   );
 };
 
-export default Model;
+export default Modal;
